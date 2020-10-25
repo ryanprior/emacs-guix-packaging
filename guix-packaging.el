283e4e5c1744f7d0be0f3067c746f35e68e5b6e3
Ryan Prior <ryanprior@hey.com>
2020-10-25 14:22:22 -0500

    Adds guix-packaging--do-on-each-line

--- guix-packaging.el
+++ guix-packaging.el
@@ -68,6 +68,20 @@
       init
     current))
 
+(defun guix-packaging--do-on-each-line (func &optional start end buffer)
+  "Move point to each line between START and END (or current
+  selected region) and run FUNC."
+  (let ((start (line-number-at-pos (or start (region-beginning))))
+        (end (line-number-at-pos (or end (region-end))))
+        (buffer (or buffer (current-buffer))))
+    (with-current-buffer buffer
+      (save-mark-and-excursion
+        (set-mark nil)
+        (goto-line start)
+        (while (<= (line-number-at-pos) end)
+          (funcall func)
+          (forward-line))))))
+
 ;;;###autoload
 (defun guix-packaging-go-mod-to-org-checkbox (&optional depth buffer)
   (interactive "p")
